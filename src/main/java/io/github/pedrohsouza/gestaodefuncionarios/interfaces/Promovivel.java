b2a32c2e31d9a2f1f2c833417e5478bac13e7165
Pedro Souza <pedrohenrique150605@gmail.com>
2026-09-01 11:11:42 -0300

    Remoção dos arquivos da versão Java puro desta branch

--- src/main/java/io/github/pedrohsouza/gestaodefuncionarios/interfaces/Promovivel.java
+++
@@ -1,7 +0,0 @@
-package io.github.pedrohsouza.gestaodefuncionarios.interfaces;
-
-import io.github.pedrohsouza.gestaodefuncionarios.model.Cargo;
-
-public interface Promovivel {
-    void promover(Cargo novoCargo);
-}
